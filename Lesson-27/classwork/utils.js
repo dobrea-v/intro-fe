47faa4bb89889ab45727238cd82c21ac0c185ba3
Vladislav Dobrea <vlad.d@goparrot.ai>
2020-06-09 22:28:04 +0300

    lesson 29

---
+++ Lesson-27/classwork/utils.js
@@ -0,0 +1,9 @@
+export function dateFormatter () {
+    var now = this.date;
+    var y = now.getFullYear();
+    var m = now.getMonth() + 1;
+    var d = now.getDate();
+    var mm = m < 10 ? '0' + m : m;
+    var dd = d < 10 ? '0' + d : d;
+    return '' + y + '-' + mm + '-' + dd;
+}
---
+++ Lesson-28/classwork/index.js
@@ -0,0 +1,6 @@
+import { sum as MakeTheSum } from './sum.js';
+import SayHi, { holla } from './hello.js';
+
+console.log(SayHi('Andrei'));
+console.log(holla('Mihai'))
+console.log(MakeTheSum(1,2))
