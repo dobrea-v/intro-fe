8e114cf70f72f503a654c77fc4e59bd67590b363
Vladislav Dobrea <vlad.d@goparrot.ai>
2020-06-14 12:16:58 +0300

    added lesson-29

---
+++ Lesson-29/classwork/src/js/index.js
@@ -0,0 +1,10 @@
+import { sum } from './sum.js'; 
+import '../styles/style.scss';
+
+console.log("Proof that it's working");
+
+function main() {
+    console.log('this is main function')
+}
+main();
+sum(1,2);
